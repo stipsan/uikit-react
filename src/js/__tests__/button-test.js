56e11e9005947c54fb926f3c4bd280b9ddd0d82e
Stian Didriksen <stipsan@gmail.com>
2015-03-04 15:14:55 +0100

    adding test for [disabled]

--- src/js/__tests__/button-test.js
+++ src/js/__tests__/button-test.js
@@ -31,11 +31,26 @@ describe('Button', function() {
     );
     var button = TestUtils.findRenderedDOMComponentWithTag(container, 'button');
 
-    // Verify that it's uk-active is not there by default
+    // Verify that the class is set
     expect(button.getDOMNode().className).toContain('uk-button-primary');
+  });
+  
+  it('ignores clicks when disabled', function() {
+    var React = require('react/addons');
+    var Button = require('../button.js');
+    var TestUtils = React.addons.TestUtils;
+
+    // Render a checkbox with label in the document
+    var container = TestUtils.renderIntoDocument(
+      <Button disabled={true}>Save</Button>
+    );
+    var button = TestUtils.findRenderedDOMComponentWithTag(container, 'button');
+    
+    // Verify that it's disabled
+    expect(button.getDOMNode().disabled).toBeTruthy();
 
     // Simulate a click and verify that it is now active
     TestUtils.Simulate.click(button);
-    expect(button.getDOMNode().className).toContain('uk-active');
+    expect(button.getDOMNode().className).not.toContain('uk-active');
   });
 });
