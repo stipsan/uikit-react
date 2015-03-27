d85d1aa60cc6d986dcdfdc20def9a83401a249f1
Stian Didriksen <stipsan@gmail.com>
2015-03-27 09:19:04 +0100

    adding core page

--- docs/src/app/app-routes.jsx
+++ docs/src/app/app-routes.jsx
@@ -6,6 +6,10 @@ var {Route, Redirect, DefaultRoute, NotFoundRoute} = Router;
 // Here we define all our material-ui ReactComponents.
 var Master = require('./components/master.jsx');
 var Home = require('./components/pages/home.jsx');
+var Core = require('./components/pages/core.jsx');
+var CoreHome = require('./components/pages/core_home.jsx');
+var Buttons = require('./components/pages/buttons.jsx');
+var Close = require('./components/pages/close.jsx');
 var NotFound = require('./components/pages/404.jsx');
 
 var AppRoutes = (
@@ -14,7 +18,11 @@ var AppRoutes = (
     
     // @TODO temp
     <Route name="get-started" handler={Home} />
-    <Route name="core" handler={Home} />
+    <Route name="core" handler={Core}>
+      <DefaultRoute handler={CoreHome}/>
+      <Route name="button" handler={Buttons} />
+      <Route name="close" handler={Close} />
+    </Route>
     <Route name="components" handler={Home} />
     <Route name="customizer" handler={Home} />
     <Route name="showcase" handler={Home} />
