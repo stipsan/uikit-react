fb03252bcf21450d3b0831ae47f6c4fc5b9a4ba2
Stian Didriksen <stipsan@gmail.com>
2015-03-27 15:28:55 +0100

    close button

--- docs/src/app/components/pages/close.jsx
+++ docs/src/app/components/pages/close.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var {Close} = require('uikit');
+var Example = require('../example.jsx');
 
 var ClosePage = React.createClass({
 
@@ -11,22 +12,16 @@ var ClosePage = React.createClass({
                   <p className="uk-article-lead">
 Defines styles for a close button that can be combined with different components.
 </p>
-<Close></Close>
-<Close alt={true}></Close>
+
                   <h2 id="usage">
 <a href="#usage" className="uk-link-reset">Usage</a>
 </h2>
                   <p>
 To apply this component, add the <code>.uk-close</code> class to an <code>&lt;a&gt;</code> or <code>&lt;button&gt;</code> element.
 </p>
-                  <h3 className="tm-article-subtitle">Example</h3>
-                  <a href="#" className="uk-close" />
-                  <h3 className="tm-article-subtitle">Markup</h3>
-                  <pre>
-<code>
-&lt;a href="" class="uk-close"&gt;&lt;/a&gt;
-</code>
-</pre>
+                  <Example uikit={['Close']} codeText={`
+        <Close />
+                  `} />
                   <hr className="uk-article-divider" />
                   <h2 id="style-modifier">
 <a
@@ -38,14 +33,9 @@ Style modifier
                   <p>
 Add the <code>.uk-close-alt</code> class to apply an alternative styling to the close button.
 </p>
-                  <h3 className="tm-article-subtitle">Example</h3>
-                  <a href="#" className="uk-close uk-close-alt" />
-                  <h3 className="tm-article-subtitle">Markup</h3>
-                  <pre>
-<code>
-&lt;a href="" class="uk-close uk-close-alt"&gt;&lt;/a&gt;
-</code>
-</pre>
+                  <Example uikit={['Close']} codeText={`
+        <Close alt={true} />
+                  `} />
                   <hr className="uk-article-divider" />
                   <h2 id="close-in-alerts">
 <a
@@ -55,15 +45,12 @@ Close in alerts
 </a>
 </h2>
                   <p>This is an example of how this component is used with an alert box from the <a href="alert.html">Alert component</a>.</p>
-                  <h3 className="tm-article-subtitle">Example</h3>
-                  <div
-className="uk-alert uk-alert-success"
-data-uk-alert>
-                    <a href="#" className="uk-alert-close uk-close" />
-                    <p>
-This is a alert using an a element as close button.
-</p>
-                  </div>
+                  <Example uikit={['Close']} codeText={`
+        <div className="uk-alert uk-alert-success">
+          <Close />
+          <p>This is a alert using an a element as close button.</p>
+        </div>
+                    `} />
                   <hr className="uk-article-divider" />
                   <h2 id="close-in-modals">
 <a
