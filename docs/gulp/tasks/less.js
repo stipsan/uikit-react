d9c8084442669fa5c97cbb956a604dae8dfe932a
Stian Didriksen <stipsan@gmail.com>
2015-07-04 03:39:49 +0200

    rewrote how the uikit css is compiled

--- docs/gulp/tasks/less.js
+++ docs/gulp/tasks/less.js
@@ -4,12 +4,15 @@ var gulp = require('gulp'),
   handleErrors = require('../util/handleErrors'),
   replace = require('gulp-replace'),
   autoprefixer = require('gulp-autoprefixer'),
+  bower = require('less-plugin-bower-resolve'),
   config = require('../config').less;
 
 gulp.task('less', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
-    .pipe(less())
+    .pipe(less({
+        plugins: [bower]
+    }))
     .on('error', handleErrors)
     .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
     .pipe(sourcemaps.write())
