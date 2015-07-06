04e47a5b6e0d60072ff19b5315a29868a8f6a231
Stian Didriksen <stipsan@gmail.com>
2015-07-07 00:41:19 +0200

    es6 module syntax

--- docs/src/app/app.jsx
+++ docs/src/app/app.jsx
@@ -1,12 +1,13 @@
-var React = require('react/addons'),
-    Router = require('react-router'),
-    routes = require('./app-routes.jsx'),
-    LiveEditor = require('./live_editor.jsx');
-
+import React from 'react';
+import Router from 'react-router';
+import routes from './app-routes.jsx';
+import LiveEditor from './live_editor.jsx';
 
 // These global vars are needed for the live examples to work
 window.ReactPlayground = LiveEditor;
 window.uikit = require('uikit-react');
+
+console.log(window.uikit.version);
 
 let location = window.location.hostname !== 'uikit-react.firebaseapp.com' ? Router.HashLocation : Router.HistoryLocation;
 
