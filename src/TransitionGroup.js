0e5a06f4e66d011e6d8bf0df22ab134070353919
Stian Didriksen <stipsan@gmail.com>
2015-07-07 03:17:37 +0200

    Convince travis it can't allow this to merge to master until test cases are written.

---
+++ src/TransitionGroup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default class TransitionGroup extends React.Component {
+
+	render() {
+		return <div>Oh hai thar!</div>;
+
+	}
+};
