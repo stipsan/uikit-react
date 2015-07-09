0f7a7f4b24ccb034f5f519a36554549398c3a33f
Stian Didriksen <stipsan@gmail.com>
2015-07-09 02:01:25 +0200

    version to the export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  version: require('uikit-react/package.json').version,
   Button: require('./Button'),
   Close: require('./Close'),
   SimpleTransitionGroup: require('./SimpleTransitionGroup')
