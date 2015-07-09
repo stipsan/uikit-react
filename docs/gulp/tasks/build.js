d4a51ba26e5ee75db44d4c07b26277c61aa338b9
Stian Didriksen <stipsan@gmail.com>
2015-07-09 02:25:51 +0200

    Don't run browserSync or browserify in gulp

--- docs/gulp/tasks/build.js
+++ docs/gulp/tasks/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['browserify', 'markup', 'svgs', 'less', 'fontIcons', 'babelBrowser']);
+gulp.task('build', ['markup', 'svgs', 'less', 'fontIcons']);
