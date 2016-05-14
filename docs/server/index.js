08f49ea939b54f4092c9d19600c855f5396b967c
Stian Didriksen <stipsan@gmail.com>
2016-05-14 04:36:00 +0200

    working on getting the server part running

--- docs/server/index.js
+++ docs/server/index.js
@@ -1,2 +1,20 @@
+/* eslint import/no-extraneous-dependencies: "off" */
+
 import compressionMiddleware from 'compression'
 import express from 'express'
+
+import htmlMiddleware from './middleware/html'
+
+const app = express()
+
+// Security reasons, this should be the default in express
+app.set('x-powered-by', false)
+
+  // Making sure everything works on a Heroku + Cloudflare infrastructure
+app.enable('trust proxy')
+
+app.use(compressionMiddleware())
+app.use(express.static('public', { maxAge: 86400000 * 365, index: false }))
+app.use(htmlMiddleware())
+
+app.listen(process.env.PORT || 5000)
