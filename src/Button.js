5bd069c5ebbdd825ce455ee5864628d53b5a3a26
Stian Didriksen <stipsan@gmail.com>
2016-06-25 16:38:49 +0200

    adding mocha test for button

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,12 @@
 var React = require('react'),
-    classNames = require('classnames');
+  classNames = require('classnames')
 
 var Button = React.createClass({
-    
+
   propTypes: {
     type: React.PropTypes.string,
-    primary: React.PropTypes.bool, 
-    success: React.PropTypes.bool, 
+    primary: React.PropTypes.bool,
+    success: React.PropTypes.bool,
     danger: React.PropTypes.bool,
     link: React.PropTypes.bool,
     mini: React.PropTypes.bool,
@@ -16,18 +16,18 @@ var Button = React.createClass({
     className: React.PropTypes.string,
     onClick: React.PropTypes.oneOfType([
       React.PropTypes.bool,
-      React.PropTypes.func
-    ])
+      React.PropTypes.func,
+    ]),
   },
-    
-  getInitialState: function() {
-    return { isActive: false };
+
+  getInitialState() {
+    return { isActive: false }
   },
-  getDefaultProps: function(){
+  getDefaultProps() {
     return {
-      type: 'button', 
-      primary: false, 
-      success: false, 
+      type: 'button',
+      primary: false,
+      success: false,
       danger: false,
       link: false,
       mini: false,
@@ -35,30 +35,31 @@ var Button = React.createClass({
       large: false,
       disabled: false,
       className: '',
-      onClick: false
-    };
+      onClick: false,
+    }
   },
-  onClick: function(event) {
-    this.setState({isActive: !this.state.isActive});
+  onClick(event) {
+    if (!this.props.disabled) {
+      this.setState({ isActive: !this.state.isActive })
+    }
   },
-  render: function() {
-    
+  render() {
     var props = this.props,
-        classes = classNames('uk-button', this.props.className, {
-          "uk-button-primary": this.props.primary, 
-          "uk-button-success": this.props.success, 
-          "uk-button-danger": this.props.danger,
-          "uk-button-link": this.props.link,
-          "uk-button-mini": this.props.mini,
-          "uk-button-small": this.props.small,
-          "uk-button-large": this.props.large,
-          "uk-active": this.state.isActive
-        });
+      classes = classNames('uk-button', this.props.className, {
+        'uk-button-primary': this.props.primary,
+        'uk-button-success': this.props.success,
+        'uk-button-danger': this.props.danger,
+        'uk-button-link': this.props.link,
+        'uk-button-mini': this.props.mini,
+        'uk-button-small': this.props.small,
+        'uk-button-large': this.props.large,
+        'uk-active': this.state.isActive,
+      })
 
     return (
         <button {...props} onClick={this.props.onClick || this.onClick} className={classes} />
-    );
-  }
-});
+    )
+  },
+})
 
-module.exports = Button;
+module.exports = Button
--- test/Button.js
+++ test/Button.js
@@ -1,18 +1,58 @@
 import React from 'react'
+import expect, { createSpy } from 'expect'
+import { shallow } from 'enzyme'
+
 import Button from '../src/Button'
 
-describe('Button', () => {
-  it('toggles the className uk-active after click')
+describe('<Button />', () => {
+  it('toggles the className uk-active after click', () => {
+    const wrapper = shallow(<Button />)
 
-  it('toggles classNames based on props')
+    expect(wrapper.is('button.uk-button')).toBe(true)
 
-  it('ignores clicks when disabled')
+    wrapper.simulate('click')
+    expect(wrapper.hasClass('uk-active')).toBe(true)
+  })
 
-  it('can be a submit type button')
+  it('toggles classNames based on props', () => {
+    const wrapper = shallow(<Button primary />)
 
-  it('can attach custom props')
+    expect(wrapper.hasClass('uk-button-primary')).toBe(true)
+  })
 
-  it('supports additional event handlers, like onMouseMove')
+  it('ignores clicks when disabled', () => {
+    const wrapper = shallow(<Button disabled />)
 
-  it('allows onClick overrides')
+    wrapper.simulate('click')
+    expect(wrapper.hasClass('uk-active')).toBe(false)
+  })
+
+  it('can be a submit type button', () => {
+    const wrapper = shallow(<Button type="submit" />)
+
+    expect(wrapper.html()).toBe('<button type="submit" class="uk-button"></button>')
+  })
+
+  it('can attach custom props', () => {
+    const wrapper = shallow(<Button id="submit" />)
+
+    expect(wrapper.html()).toBe('<button id="submit" type="button" class="uk-button"></button>')
+  })
+
+  it('supports additional event handlers, like onMouseMove', () => {
+    const spy = createSpy()
+    const wrapper = shallow(<Button onMouseMove={spy} />)
+
+    wrapper.simulate('mousemove')
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it('allows onClick overrides', () => {
+    const spy = createSpy()
+    const wrapper = shallow(<Button onClick={spy} />)
+
+    wrapper.simulate('click')
+    expect(spy).toHaveBeenCalled()
+    expect(wrapper.hasClass('uk-active')).toBe(false)
+  })
 })
