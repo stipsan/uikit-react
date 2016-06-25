b90b32d56f80bbc72f62b11e086915b275561dbb
Stian Didriksen <stipsan@gmail.com>
2016-06-25 15:56:51 +0200

    test placeholder setup

---
+++ test/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../src/Button'
+
+describe('Button', () => {
+  it('toggles the className uk-active after click')
+
+  it('toggles classNames based on props')
+
+  it('ignores clicks when disabled')
+
+  it('can be a submit type button')
+
+  it('can attach custom props')
+
+  it('supports additional event handlers, like onMouseMove')
+
+  it('allows onClick overrides')
+})
