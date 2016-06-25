a2250431243ab0d8102fc567760bd5db771ad520
Stian Didriksen <stipsan@gmail.com>
2016-06-25 16:39:09 +0200

    cleanup

--- gulp/tasks/fontIcons.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').fontIcons;
-
-gulp.task('fontIcons', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest.development + config.folder))
-    .pipe(gulp.dest(config.dest.production + config.folder));
-});
--- gulp/tasks/svgs.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').svgs;
-
-gulp.task('svgs', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest.development + config.folder))
-    .pipe(gulp.dest(config.dest.production + config.folder));
-});
