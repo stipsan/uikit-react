a087b734c2323cc6b8e1f872dd6e7d8c759facf1
Stian Didriksen <stipsan@gmail.com>
2016-06-26 17:32:05 +0200

    added default props test

--- test/Dropdown.js
+++ test/Dropdown.js
@@ -5,6 +5,22 @@ import { shallow } from 'enzyme'
 import Dropdown from '../src/Dropdown'
 
 describe('<Dropdown />', () => {
+  it('should mirror the same options provided in the official jQuery plugin', () => {
+    const wrapper = shallow(<Dropdown />)
+
+    expect(wrapper.instance().props).toEqual({
+      mode: 'hover',
+      pos: 'bottom-left',
+      offset: 0,
+      remaintime: 800,
+      justify: false,
+      boundary: window,
+      delay: 0,
+      hoverDelayIdle: 250,
+      preventflip: false,
+    })
+  })
+
   it('toggles the className uk-open after mouse hover', () => {
     const wrapper = shallow(<Dropdown mode="hover" />)
 
