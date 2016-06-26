9b39792b8b0d421c48901e0cfdcf45a64cd8d87d
Stian Didriksen <stipsan@gmail.com>
2016-06-26 17:31:47 +0200

    changed import statement to follow convention

--- src/stories/dropdown.js
+++ src/stories/dropdown.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
 
-import Dropdown from '../DropDown'
+import { Dropdown } from '../index'
 
 storiesOf('Dropdown', module)
   .add('with text', () => (
