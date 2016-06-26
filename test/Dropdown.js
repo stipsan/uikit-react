a06bdeae51b4e93b5190cfb77db97789bc310402
Stian Didriksen <stipsan@gmail.com>
2016-06-26 18:07:02 +0200

    added some more tests

--- test/Dropdown.js
+++ test/Dropdown.js
@@ -21,6 +21,24 @@ describe('<Dropdown />', () => {
     })
   })
 
+  it('should require a label prop', () => {
+    expect(Dropdown.propTypes.label).toEqual(React.PropTypes.string.isRequired)
+  })
+
+  it('should have a state isOpen that toggles .uk-open', () => {
+    const wrapper = shallow(<Dropdown />)
+
+    expect(wrapper.state()).toEqual({
+      isOpen: false,
+    })
+
+    wrapper.setState({ isOpen: true })
+    expect(wrapper.hasClass('uk-open')).toBe(true, 'Setting state isOpen = true adds .uk-open')
+
+    wrapper.setState({ isOpen: false })
+    expect(wrapper.hasClass('uk-open')).toBe(false, 'Setting state isOpen = false removes .uk-open')
+  })
+
   it('toggles the className uk-open after mouse hover', () => {
     const wrapper = shallow(<Dropdown mode="hover" />)
 
@@ -38,7 +56,7 @@ describe('<Dropdown />', () => {
     expect(wrapper.hasClass('uk-open')).toBe(true)
   })
 
-  it('toggles the class name uk-open after mouse hover after 1000 second', () => {
+  it('toggles the class name uk-open after mouse hover after 100 milliseconds', () => {
     const wrapper = shallow(<Dropdown delay={100} />)
 
     expect(wrapper.hasClass('uk-button-dropdown')).toBe(true)
@@ -50,6 +68,41 @@ describe('<Dropdown />', () => {
         wrapper.simulate('mouseenter')
         resolve(expect(wrapper.hasClass('uk-open')).toBe(true))
       }, 100)
+    })
+  })
+
+  it('should delay removal of uk-open to the timeout in remaintime prop', () => {
+    const wrapper = shallow(<Dropdown remaintime={100} />)
+
+    wrapper.simulate('mouseenter')
+    expect(wrapper.hasClass('uk-open')).toBe(true)
+
+    wrapper.simulate('mouseleave')
+    expect(wrapper.hasClass('uk-open')).toBe(true, '.uk-open was removed before remaintime timeout')
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(expect(wrapper.hasClass('uk-open')).toBe(false))
+      }, 200)
+    })
+  })
+
+  it('should cancel removal of uk-open on mouseenter', () => {
+    const wrapper = shallow(<Dropdown remaintime={100} />)
+
+    wrapper.simulate('mouseenter')
+    expect(wrapper.hasClass('uk-open')).toBe(true)
+
+    wrapper.simulate('mouseleave')
+    expect(wrapper.hasClass('uk-open')).toBe(true, '.uk-open was removed before remaintime timeout')
+    wrapper.simulate('mouseenter')
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(
+          expect(wrapper.hasClass('uk-open')).toBe(true, 'failed to cancel remaintime timeout')
+        )
+      }, 200)
     })
   })
 })
