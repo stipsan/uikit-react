9503a41ebf365ee42c82921bb852af4897d10384
Stian Didriksen <stipsan@gmail.com>
2016-07-31 15:53:57 +0200

    Setting up storybooks for uikit-react.io

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
   resolve: {
     root: path.join(__dirname, '..'),
     alias: {
--- src/stories/button.js
+++ src/stories/button.js
@@ -1,14 +1,20 @@
-import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { Button } from 'uikit-react'
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <div className="uk-container uk-margin-top">
+  .addWithInfo(
+    'Basic Usage',
+    `
+      This is the basic usage with the button with providing a label to show the text.
+    `,
+    () => (
+    <div>
       <Button primary>Submit</Button> &nbsp;
       <Button success>Submit</Button> &nbsp;
       <Button danger>Submit</Button> &nbsp;
       <Button link>Submit</Button> &nbsp;
       <Button type="submit">Submit</Button>
     </div>
-  ))
+  ),
+  { inline: true, propTables: [Button] }
+)
