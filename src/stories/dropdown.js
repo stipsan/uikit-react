3271a59ba4ffb38e01f45acdfed4f7008c60ef89
Stian Didriksen <stipsan@gmail.com>
2016-08-01 18:54:54 +0200

    minor cleanup

--- src/stories/dropdown.js
+++ src/stories/dropdown.js
@@ -2,48 +2,42 @@ import { storiesOf, action } from '@kadira/storybook'
 
 import Dropdown from '../Dropdown'
 
+const Menu = () => (
+  <div className="uk-dropdown uk-dropdown-bottom">
+    <ul className="uk-nav uk-nav-dropdown">
+      <li><a className="uk-warning" href="#">Item</a></li>
+      <li><a href="#">Another item</a></li>
+      <li className="uk-nav-header">Header</li>
+      <li><a href="#">Item</a></li>
+      <li><a href="#">Another item</a></li>
+      <li className="uk-nav-divider"></li>
+      <li><a href="#">Separated item</a></li>
+    </ul>
+  </div>
+)
+
 storiesOf('Dropdown', module)
   .addWithInfo(
     'Basic Usage',
     `
-      This is the basic usage with the button with providing a label to show the text.
+      This is the basic usage showing hover, and click mode.
     `,
     () => (
       <div className="">
         <Dropdown>
-          <button className="uk-button" onMouseOver={action('onMouseOver')}>
+          <button className="uk-button">
             Hover
             <i className="uk-icon-caret-down"></i>
           </button>
-          <div className="uk-dropdown uk-dropdown-bottom">
-            <ul className="uk-nav uk-nav-dropdown">
-              <li><a className="uk-warning" href="#">Item</a></li>
-              <li><a href="#">Another item</a></li>
-              <li className="uk-nav-header">Header</li>
-              <li><a href="#">Item</a></li>
-              <li><a href="#">Another item</a></li>
-              <li className="uk-nav-divider"></li>
-              <li><a href="#">Separated item</a></li>
-            </ul>
-          </div>
+          <Menu />
         </Dropdown>
         <br /> <br />
         <Dropdown mode="click">
-          <button className="uk-button" onClick={action('onClick')}>
+          <button className="uk-button">
             Click me
             <i className="uk-icon-caret-down"></i>
           </button>
-          <div className="uk-dropdown uk-dropdown-bottom">
-            <ul className="uk-nav uk-nav-dropdown">
-              <li><a className="uk-warning" href="#">Item</a></li>
-              <li><a href="#">Another item</a></li>
-              <li className="uk-nav-header">Header</li>
-              <li><a href="#">Item</a></li>
-              <li><a href="#">Another item</a></li>
-              <li className="uk-nav-divider"></li>
-              <li><a href="#">Separated item</a></li>
-            </ul>
-          </div>
+          <Menu />
         </Dropdown>
       </div>
   ),
