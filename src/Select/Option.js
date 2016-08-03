93aaf36246d453bb8565fd458bc6d3eeab00a27a
Stian Didriksen <stipsan@gmail.com>
2016-08-03 18:41:40 +0200

    convert Select/Option to es6 class

--- src/Select/Option.js
+++ src/Select/Option.js
@@ -1,23 +1,23 @@
 import classNames from 'classnames'
-import React from 'react'
+import { Component, PropTypes } from 'react'
 
-const Option = React.createClass({
-  propTypes: {
-    addLabelText: React.PropTypes.string,               // text to display with value while creating new option
-    children: React.PropTypes.node,
-    className: React.PropTypes.string,                  // className (based on mouse position)
-    instancePrefix: React.PropTypes.string.isRequired,  // unique prefix for the ids (used for aria)
-    isDisabled: React.PropTypes.bool,                   // the option is disabled
-    isFocused: React.PropTypes.bool,                    // the option is focused
-    isSelected: React.PropTypes.bool,                   // the option is selected
-    onFocus: React.PropTypes.func,                      // method to handle mouseEnter on option element
-    onSelect: React.PropTypes.func,                     // method to handle click on option element
-    onUnfocus: React.PropTypes.func,                    // method to handle mouseLeave on option element
-    option: React.PropTypes.object.isRequired,          // object that is base for that option
-    optionIndex: React.PropTypes.number,                 // index of the option, used to generate unique ids for aria
-  },
+export default class Option extends Component {
+  static propTypes = {
+    addLabelText: PropTypes.string,               // text to display with value while creating
+    children: PropTypes.node,
+    className: PropTypes.string,                  // className (based on mouse position)
+    instancePrefix: PropTypes.string.isRequired,  // unique prefix for the ids (used for aria)
+    isDisabled: PropTypes.bool,                   // the option is disabled
+    isFocused: PropTypes.bool,                    // the option is focused
+    isSelected: PropTypes.bool,                   // the option is selected
+    onFocus: PropTypes.func,                      // method to handle mouseEnter on option
+    onSelect: PropTypes.func,                     // method to handle click on option element
+    onUnfocus: PropTypes.func,                    // method to handle mouseLeave on option
+    option: PropTypes.object.isRequired,          // object that is base for that option
+    optionIndex: PropTypes.number,                // index of the option, used to generate
+  }
 
-  blockEvent(event) {
+  handleBlockEvent = (event) => {
     event.preventDefault()
     event.stopPropagation()
     if ((event.target.tagName !== 'A') || !('href' in event.target)) {
@@ -28,57 +28,59 @@ const Option = React.createClass({
     } else {
       window.location.href = event.target.href
     }
-  },
+  }
 
-  handleMouseDown(event) {
+  handleMouseDown = (event) => {
     event.preventDefault()
     event.stopPropagation()
     this.props.onSelect(this.props.option, event)
-  },
+  }
 
-  handleMouseEnter(event) {
+  handleMouseEnter = (event) => {
     this.onFocus(event)
-  },
+  }
 
-  handleMouseMove(event) {
+  handleMouseMove = (event) => {
     this.onFocus(event)
-  },
+  }
 
-  handleTouchEnd(event) {
+  handleTouchEnd= (event) => {
     // Check if the view is being dragged, In this case
     // we don't want to fire the click event (because the user only wants to scroll)
     if (this.dragging) return
 
     this.handleMouseDown(event)
-  },
+  }
 
-  handleTouchMove(event) {
+  handleTouchMove = () => {
     // Set a flag that the view is being dragged
     this.dragging = true
-  },
+  }
 
-  handleTouchStart(event) {
+  handleTouchStart = () => {
     // Set a flag that the view is not being dragged
     this.dragging = false
-  },
+  }
 
-  onFocus(event) {
+  onFocus = (event) => {
     if (!this.props.isFocused) {
       this.props.onFocus(this.props.option, event)
     }
-  },
+  }
   render() {
-    let { option, instancePrefix, optionIndex } = this.props
+    const { option, instancePrefix, optionIndex } = this.props
     let className = classNames(this.props.className, option.className)
     return option.disabled ? (
-      <div className={className}
+      <div
+        className={className}
         onMouseDown={this.blockEvent}
-        onClick={this.blockEvent}
+        onClick={this.handleBlockEvent}
       >
         {this.props.children}
       </div>
     ) : (
-      <div className={className}
+      <div
+        className={className}
         style={option.style}
         role="option"
         onMouseDown={this.handleMouseDown}
@@ -87,13 +89,15 @@ const Option = React.createClass({
         onTouchStart={this.handleTouchStart}
         onTouchMove={this.handleTouchMove}
         onTouchEnd={this.handleTouchEnd}
-        id={instancePrefix + '-option-' + optionIndex}
+        id={`${instancePrefix}-option-${optionIndex}`}
         title={option.title}
       >
-        { option.create ? this.props.addLabelText.replace('{label}', option.label) : this.props.children }
+        {
+          option.create ?
+          this.props.addLabelText.replace('{label}', option.label) :
+          this.props.children
+        }
       </div>
     )
-  },
-})
-
-module.exports = Option
+  }
+}
