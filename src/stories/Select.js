a297fc87fca705fe82d794f4655911ba2f5ac410
Stian Didriksen <stipsan@gmail.com>
2016-08-03 15:58:48 +0200

    initial work on react-select fork

---
+++ src/stories/Select.js
@@ -0,0 +1,16 @@
+import { storiesOf } from '@kadira/storybook'
+import { Select } from 'uikit-react'
+
+storiesOf('Select', module)
+  .addWithInfo(
+    'Basic Usage',
+    `
+      This is the basic usage examples
+    `,
+    () => (
+      <div>
+        <Select options={['Color', 'Size', 'Material']} />
+      </div>
+  ),
+  { inline: true, propTables: [Select] }
+)
