3e2a467b43b958fc71ac55f7c7751cbfac6c8e2c
Stian Didriksen <stipsan@gmail.com>
2016-08-14 19:43:10 +0200

    refactoring stories

--- src/stories/Modal.js
+++ src/stories/Modal.js
@@ -1,4 +1,4 @@
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/storybook'
 import { Component } from 'react'
 import { Button } from 'uikit-react'
 import { Modal } from 'uikit-react'
@@ -6,9 +6,35 @@ import { Modal } from 'uikit-react'
 Modal.displayName = 'Modal'
 
 class OpenButton extends Component {
+  handleClick = () => {
+    action('handleOpen')()
+    this.props.handleOpen()
+  }
   render() {
-    const { handleOpen } = this.props
-    return <Button onClick={handleOpen}>Open</Button>
+    const { handleClick } = this
+    return <Button onClick={handleClick}>Open</Button>
+  }
+}
+
+class CancelButton extends Component {
+  handleClick = () => {
+    action('handleCancel')()
+    this.props.handleClose()
+  }
+  render() {
+    const { handleClick } = this
+    return <Button onClick={handleClick}>Cancel</Button>
+  }
+}
+
+class SaveButton extends Component {
+  handleClick = () => {
+    action('handleSave')()
+    this.props.handleClose()
+  }
+  render() {
+    const { handleClick } = this
+    return <Button primary onClick={handleClick}>Save</Button>
   }
 }
 
@@ -34,24 +60,9 @@ storiesOf('Modal', module)
         headline="This is the Headline"
         target={OpenButton}
         header="Headline"
-        footer={[
-          <button type="button" className="uk-button">Cancel</button>,
-          <button type="button" className="uk-button uk-button-primary">Save</button>,
-        ]}
+        footer={[CancelButton, SaveButton]}
       >
-        <div className="uk-modal-header">
-          <h2>Headline</h2>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div className="uk-modal-footer uk-text-right">
-          <button type="button" className="uk-button">Cancel</button>
-          <button type="button" className="uk-button uk-button-primary">Save</button>
-        </div>
+        <p>{LoremIpsum}</p>
       </Modal>
     </div>
   ), { header: false, inline: true })
