50dde985b056687fccfe09a61c0786535256660a
Stian Didriksen <stipsan@gmail.com>
2016-08-14 19:32:08 +0200

    rename back

--- src/stories/Modal.js
+++ src/stories/Modal.js
@@ -12,8 +12,7 @@ class OpenButton extends Component {
   }
 }
 
-const LoremIpsum = `
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+const LoremIpsum = `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
