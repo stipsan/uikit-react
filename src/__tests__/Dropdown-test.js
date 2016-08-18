873ea6a656604c331198e4931ddeee14726e478d
Stian Didriksen <stipsan@me.com>
2016-08-18 23:12:09 +0200

    Test cleanup (#155)
    * test cleanup
    
    * dropdown jest test
    
    * Better test coverage in modal

---
+++ src/__tests__/Dropdown-test.js
@@ -0,0 +1,68 @@
+import renderer from 'react-test-renderer'
+
+import Dropdown from '../Dropdown'
+
+describe('Dropdown', () => {
+  it('renders correctly', () => {
+    expect(renderer.create(
+      <Dropdown>
+        <button className="uk-button">Hover <i className="uk-icon-caret-down" /></button>
+      </Dropdown>
+    ).toJSON()).toMatchSnapshot()
+
+    expect(renderer.create(
+      <Dropdown className="uk-parent" component="li" >
+        <a>Parent <i className="uk-icon-caret-down" /></a>
+      </Dropdown>
+    ).toJSON()).toMatchSnapshot()
+  })
+
+  it('changes the class when hovered', () => {
+    const component = renderer.create(
+      <Dropdown>
+        <button className="uk-button">Hover <i className="uk-icon-caret-down" /></button>
+      </Dropdown>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+    // manually trigger the callback
+    tree.props.onMouseEnter()
+    // re-rendering
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+    // manually trigger the callback
+    tree.props.onMouseLeave()
+    // re-rendering
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+    // Fast forward timers, class should now be gone
+    jest.runAllTimers()
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('changes the class when clicked', () => {
+    const component = renderer.create(
+      <Dropdown mode="click">
+        <button className="uk-button">Hover <i className="uk-icon-caret-down" /></button>
+      </Dropdown>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+    // manually trigger the callback
+    tree.props.onClick()
+    // re-rendering
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+    // manually trigger the callback
+    tree.props.onClick()
+    // re-rendering
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
