b4ea11b03992d2b20c91b71b0d141a3ff5c8b0c5
sureshHARDIYA <itsmeskm99@gmail.com>
2016-11-09 21:34:48 +0100

    add test

---
+++ src/Notify/__tests__/index-test.js
@@ -0,0 +1,26 @@
+import renderer from 'react-test-renderer'
+
+import Notify from '../index'
+
+jest.mock('react-portal', () => 'Portal')
+
+describe('Notify', () => {
+  const defaultProps = {
+    notifications: [{ id: 1, type: 'warning', message: 'Notification #1' }],
+    icon: 'check',
+    isSticky: false,
+    position: 'top-center',
+  }
+  const component = renderer.create(
+    <Notify {...defaultProps} />
+  )
+  it('should render correctly', () => {
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should hanlde click correctly', () => {
+    const instance = component.getInstance()
+    instance.handleClick()
+    expect(instance.state.isOpened).toBe(true)
+  })
+})
