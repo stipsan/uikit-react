7cbca299de267596ee6a2ca44079cda94e4264cf
sureshHARDIYA <itsmeskm99@gmail.com>
2016-12-20 14:57:30 +0100

    update the typo

--- src/Notify/__tests__/index-test.js
+++ src/Notify/__tests__/index-test.js
@@ -6,11 +6,8 @@ jest.mock('react-portal', () => 'Portal')
 
 describe('Notify', () => {
   const defaultProps = {
-    notifications: [{ id: 1, type: 'warning', message: 'Notification #1' }],
-    icon: 'check',
-    isSticky: false,
+    notifications: [{ id: 1, type: 'warning', message: 'Notification #1', icon: 'check' }],
     position: 'top-center',
-    handleMessageClick: jest.fn(),
   }
   const component = renderer.create(
     <Notify {...defaultProps} />
