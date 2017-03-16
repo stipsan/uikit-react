e72ca1ce832192b05e43d92e597ad67bfb9e6da5
Stian Didriksen <stipsan@gmail.com>
2017-03-16 18:41:35 +0100

    test guard (it should block the pr)

--- guard.js
+++ guard.js
@@ -3,10 +3,11 @@ const semver = require('semver')
 const semverTruncate = require('semver-truncate')
 
 const semverRange = '<=2'
+const branch = process.env.TRAVIS_BRANCH
 
-console.log('TRAVIS_PULL_REQUEST_BRANCH', process.env.TRAVIS_PULL_REQUEST_BRANCH)
+console.log('TRAVIS_PULL_REQUEST_BRANCH', process.env.TRAVIS_PULL_REQUEST_BRANCH, 'TRAVIS_BRANCH', process.env.TRAVIS_BRANCH)
 
-if (!semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
+if (branch === 'master' && !semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
   const err = new Error(`uikit ${data.version} doesn't satisfy the version requirement of ${semverRange}`)
   err.name = 'InvalidUikitVersion'
   throw err
