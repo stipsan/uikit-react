9adfe142be3c8799c911b6fa103e56a7823f17d2
Stian Didriksen <stipsan@me.com>
2017-03-16 18:58:53 +0100

    adding a guard (#477)
    * adding a guard
    
    * testing travis
    
    * testing env
    
    * test env
    
    * test guard (it should block the pr)
    
    * test guard (it should pass)
    
    * remove
    
    * add dependencies in the right place
    
    * remove junk dep

---
+++ guard.js
@@ -0,0 +1,14 @@
+const data = require('uikit/package.json')
+const semver = require('semver')
+const semverTruncate = require('semver-truncate')
+
+const semverRange = '<=2'
+const branch = process.env.TRAVIS_BRANCH
+
+console.log('TRAVIS_PULL_REQUEST_BRANCH', process.env.TRAVIS_PULL_REQUEST_BRANCH, 'TRAVIS_BRANCH', process.env.TRAVIS_BRANCH)
+
+if (branch === 'master' && !semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
+  const err = new Error(`uikit ${data.version} doesn't satisfy the version requirement of ${semverRange}`)
+  err.name = 'InvalidUikitVersion'
+  throw err
+}
