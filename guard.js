d1f88812868ec7ef983ece9a19dbb7dfe589122b
Stian Didriksen <stipsan@gmail.com>
2017-03-16 15:59:33 +0100

    testing travis

--- guard.js
+++ guard.js
@@ -4,6 +4,8 @@ const semverTruncate = require('semver-truncate')
 
 const semverRange = '<=2'
 
+console.log('TRAVIS_PULL_REQUEST_BRANCH', process.env.TRAVIS_PULL_REQUEST_BRANCH)
+
 if (!semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
   const err = new Error(`uikit ${data.version} doesn't satisfy the version requirement of ${semverRange}`)
   err.name = 'InvalidUikitVersion'
