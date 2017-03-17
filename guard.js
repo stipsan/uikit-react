07f466efb1ad533c3aef97050009b548216d58b7
Stian Didriksen <stipsan@gmail.com>
2017-03-17 11:49:59 +0100

    update guard script to only apply on PRs

--- guard.js
+++ guard.js
@@ -3,11 +3,12 @@ const semver = require('semver')
 const semverTruncate = require('semver-truncate')
 
 const semverRange = '<=2'
+const pr = process.env.TRAVIS_PULL_REQUEST_BRANCH
 const branch = process.env.TRAVIS_BRANCH
 
 console.log('TRAVIS_PULL_REQUEST_BRANCH', process.env.TRAVIS_PULL_REQUEST_BRANCH, 'TRAVIS_BRANCH', process.env.TRAVIS_BRANCH)
 
-if (branch === 'master' && !semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
+if (pr && branch === 'master' && !semver.satisfies(semverTruncate(data.version, 'patch'), semverRange)) {
   const err = new Error(`uikit ${data.version} doesn't satisfy the version requirement of ${semverRange}`)
   err.name = 'InvalidUikitVersion'
   throw err
