1e12a61fdaf1abfdeb1a091f414f695b3ec34ad1
sureshHARDIYA <itsmeskm99@gmail.com>
2017-03-21 22:53:49 +0100

    fix linting

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -7,30 +7,43 @@ Button.displayName = 'Button'
 storiesOf('Button', module)
   .addWithInfo('Basic Usage', '', () => (
     <div className="uk-margin-bottom">
-      <Button onClick={action('onClick Default')}>Default</Button> &nbsp;
-      <Button disabled>Default Disabled</Button> &nbsp;
+      <Button onClick={action('onClick Default')}>Default</Button>
+      &nbsp;
+      <Button disabled>Default Disabled</Button>
     </div>
   ), { header: false, inline: true, propTables: [Button] })
 
   .addWithInfo('Style modifiers', '', () => (
     <div className="uk-margin-bottom">
-      <Button onClick={action('onClick Default')}>Default</Button> &nbsp;
-      <Button primary onClick={action('onClick Primary')}>Primary</Button> &nbsp;
-      <Button secondary onClick={action('onClick Primary')}>Secondary</Button> &nbsp;
-      <Button danger onClick={action('onClick Danger')}>Danger</Button> &nbsp;
-      <Button text onClick={action('onClick Danger')}>Text</Button> &nbsp;
-      <Button link onClick={action('onClick Link')}>Link</Button> &nbsp;
+      <Button onClick={action('onClick Default')}>Default</Button>
+      &nbsp;
+      <Button primary onClick={action('onClick Primary')}>Primary</Button>
+      &nbsp;
+      <Button secondary onClick={action('onClick Primary')}>Secondary</Button>
+      &nbsp;
+      <Button danger onClick={action('onClick Danger')}>Danger</Button>
+      &nbsp;
+      <Button text onClick={action('onClick Danger')}>Text</Button>
+      &nbsp;
+      <Button link onClick={action('onClick Link')}>Link</Button>
+      &nbsp;
     </div>
   ), { header: false, inline: true, propTables: [Button] })
 
   .addWithInfo('Size modifiers', '', () => (
     <div className="uk-margin-bottom">
-      <Button small onClick={action('onClick Success')}>SMALL BUTTON</Button> &nbsp;
-      <Button primary small onClick={action('onClick Success')}>SMALL BUTTON</Button> &nbsp;
-      <Button secondary small onClick={action('onClick Success')}>SMALL BUTTON</Button> &nbsp;
+      <Button small onClick={action('onClick Success')}>SMALL BUTTON</Button>
+      &nbsp;
+      <Button primary small onClick={action('onClick Success')}>SMALL BUTTON</Button>
+      &nbsp;
+      <Button secondary small onClick={action('onClick Success')}>SMALL BUTTON</Button>
+      &nbsp;
       <br /><br />
-      <Button large onClick={action('onClick Submit')}>LARGE BUTTON</Button> &nbsp;
-      <Button large primary onClick={action('onClick Submit')}>LARGE BUTTON</Button> &nbsp;
-      <Button large secondary onClick={action('onClick Submit')}>LARGE BUTTON</Button> &nbsp;
+      <Button large onClick={action('onClick Submit')}>LARGE BUTTON</Button>
+      &nbsp;
+      <Button large primary onClick={action('onClick Submit')}>LARGE BUTTON</Button>
+      &nbsp;
+      <Button large secondary onClick={action('onClick Submit')}>LARGE BUTTON</Button>
+      &nbsp;
     </div>
   ), { header: false, inline: true, propTables: [Button] })
