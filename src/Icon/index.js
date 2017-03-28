87ff43e5f8e3d34ad824fb434325fa593b6a1a1e
Stian Didriksen <stipsan@gmail.com>
2017-03-28 21:02:59 +0200

    preserves original viewBox

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,55 +1,19 @@
-import { Component, PropTypes } from 'react'
-import cx from 'classnames'
+import { PropTypes } from 'react'
 
-import icons from './icons'
+import Default from './Default'
 import Spinner from './Spinner'
-console.log(icons)
-const iconNames = Object.keys(icons)
 
-export default class Icon extends Component {
-
-  static propTypes = {
-    icon: PropTypes.oneOf([...iconNames, 'spinner']).isRequired,
-    ratio: PropTypes.number.isRequired,
-    // @TODO custom viewBox validator
-    viewBox: PropTypes.string.isRequired,
-    className: PropTypes.string,
-  }
-
-  static defaultProps = {
-    className: '',
-    ratio: 1,
-    viewBox: '0 0 20 20',
-  }
-
-  render() {
-    // @TODO parse icon size from viewbox
-    const height = 20
-    const width = 20
-
-    const { className, ratio, viewBox, icon, ...props } = this.props
-
-    if (icon === 'spinner') {
-      return <Spinner className={className} />
-    }
-
-    const svg = icons[icon] && icons[icon].replace(/^<svg[^>]*>|<\/svg>$/g, '')
-
-    return (
-      <span className={cx('uk-icon', className)}>
-        <svg
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: svg }}
-          {...props}
-          // @TODO optimize
-
-          height={height * ratio}
-          viewBox={viewBox}
-          icon={icon}
-          width={width * ratio}
-          xmlns="http://www.w3.org/2000/svg"
-        />
-      </span>
-    )
+const Icon = (props) => {
+  switch (props.icon) {
+    case 'spinner':
+      return <Spinner {...props} />
+    default:
+      return <Default {...props} />
   }
 }
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+}
+
+export default Icon
--- src/Icon/utils.js
+++ src/Icon/utils.js
@@ -1,9 +1,13 @@
 
 
-export const getViewbox = (svg) => {
+export const getDimensionsFromViewBox = (svg) => {
   const match = svg.match(/view[bB]ox=['"]([\s\S]*?)['"]/m)
 
-  return match && match[1]
+  if (!match) return false
+
+  const [, , width, height] = match[1].split(' ')
+
+  return { width, height }
 }
 
-export const trimSvg = svg => svg.replace(/^<svg[^>]*>|<\/svg>$/g, '')
+export const getInnerSvg = svg => svg.replace(/^<svg[^>]*>|<\/svg>$/g, '')
