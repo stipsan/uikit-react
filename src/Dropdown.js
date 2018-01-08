8529ec93e1a2aaef6258e27909b077d90c3f00d4
Stian Didriksen <stipsan@gmail.com>
2018-01-08 12:38:11 +0100

    typo

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import Recact, { Component, cloneElement } from 'react'
+import React, { Component, cloneElement } from 'react'
 import PropTypes from 'prop-types'
 
 const links = {}
